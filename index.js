17778f33c9341d3aaca972f1a30fdf364acdc153
Sheena Sang <sheenasany@gmail.com>
2022-07-19 09:33:24 -0400

    commit tree acc6bc4f4f6f14b7a16c025a4edef7e407dca212

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    return Math.abs(42 - pickupLocation)
+}
+
+function distanceFromHqInFeet(someValue) {
+    return 264 * distanceFromHqInBlocks(someValue)
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    let distanceInFeet = distanceTravelledInFeet(start, destination)
+    let fare;
+    // create an if statement to show what the fare would be depending on the distance traveled in fee
+    if (distanceInFeet < 400) {
+        fare = 0
+    } 
+    else if (distanceInFeet >= 400 && distanceInFeet <= 2000) {
+      fare = (distanceInFeet - 400) * .02;
+    } 
+    else if (distanceInFeet > 2000 && distanceInFeet < 2500) {
+        fare = 25 
+    } 
+    else {
+        fare = "cannot travel that far"
+    }
+    return fare;
+}
